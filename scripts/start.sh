ca38eec34b5413afb321bf2fbfe25e3de564be6d
Djoncarton1921 <shapranov.nik@gmail.com>
2023-06-02 12:07:36 +0300

    BLAN-1307 - created base project setup

---
+++ scripts/start.sh
@@ -0,0 +1,6 @@
+cd chat
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+python manage.py loaddata */fixtures/*.json
+python manage.py runserver 0.0.0.0:12000
